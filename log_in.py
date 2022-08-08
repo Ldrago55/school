cc5decd7de007564d02cb58b73228f74653ad9a8
Ldrago55 <bjana2580@gmail.com>
2022-08-08 10:12:34 +0530

    webbot commit

--- log_in.py
+++ log_in.py
@@ -1,3 +1,4 @@
+<form name="loginform" method="post" action="userinfo.php">
 import requests
 url = 'http://example.com/userinfo.php'
 values = {'username': 'user',
@@ -5,3 +6,13 @@ values = {'username': 'user',
 
 r = requests.post(url, data=values)
 # print r.content
+
+
+from webbot import Browser 
+web = Browser()
+web.go_to('google.com') 
+web.click('Sign in')
+web.type('mymail@gmail.com' , into='Email')
+web.click('NEXT' , tag='span')
+web.type('mypassword' , into='Password' , id='passwordFieldId') # specific selection
+web.click('NEXT' , tag='span') # you are logged in ^_^
